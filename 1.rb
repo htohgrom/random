c5b8dab5d1aec17ff6519cd63617c87fa7abf2c2
Krystian Wilk Pasterczyk <htohgrom+git@gmail.com>
2017-12-01 20:37:54 +0100

    Simplied the code.
    Simplified the solution code - it is now a single method.
    Description removed from the solution file. Will be moved to README.

--- 1.rb
+++ 1.rb
@@ -1,96 +1,42 @@
 #!/usr/bin/env ruby
-#
-# Advent of Code 2017
-#
-# --- Day 1: Inverse Captcha ---
-# 
-# The night before Christmas, one of Santa's Elves calls you in a panic. "The
-# printer's broken! We can't print the Naughty or Nice List!" By the time you
-# make it to sub-basement 17, there are only a few minutes until midnight. "We
-# have a big problem," she says; "there must be almost fifty bugs in this system,
-# but nothing else can print The List. Stand in this square, quick!  There's no
-# time to explain; if you can convince them to pay you in stars, you'll be able
-# to--" She pulls a lever and the world goes blurry.
-# 
-# When your eyes can focus again, everything seems a lot more pixelated than
-# before. She must have sent you inside the computer! You check the system clock:
-# 25 milliseconds until midnight. With that much time, you should be able to
-# collect all fifty stars by December 25th.
-# 
-# Collect stars by solving puzzles. Two puzzles will be made available on each
-# day millisecond in the advent calendar; the second puzzle is unlocked when you
-# complete the first. Each puzzle grants one star. Good luck!
-# 
-# You're standing in a room with "digitization quarantine" written in LEDs along
-# one wall. The only door is locked, but it includes a small interface.
-# "Restricted Area - Strictly No Digitized Users Allowed."
-# 
-# It goes on to explain that you may only leave by solving a captcha to prove
-# you're not a human. Apparently, you only get one millisecond to solve the
-# captcha: too fast for a normal human, but it feels like hours to you.
-# 
-# The captcha requires you to review a sequence of digits (your puzzle input) and
-# find the sum of all digits that match the next digit in the list. The list is
-# circular, so the digit after the last digit is the first digit in the list.
-# 
-# For example:
-# 
-#     * 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the
-#       second digit and the third digit (2) matches the fourth digit.
-#     * 1111 produces 4 because each digit (all 1) matches the next.
-#     * 1234 produces 0 because no digit matches the next.
-#     * 91212129 produces 9 because the only digit that matches the next one is
-#       the last digit, 9.
-# 
-# What is the solution to your captcha?
+
+def prep(input)
+  input.strip.chars.map(&:to_i)
+end
 
 def test(input, expect, fun)
-  result = fun.call(input)
+  result = fun.call(prep(input))
   result_str = result == expect ? "[ OK ]" : "[FAIL]"
   puts "#{result_str} input: #{input} result: #{result} expected: #{expect}"
 end
 
-def solve(input)
+def solver(input, opt)
   res = 0
-  (0..input.length).each do |i|
-    res += input[i].to_i if input[i] == input[i-1]
+  offset = {:part1 => 1, :part2 => input.length/2}
+  (0...input.length).each do |i|
+    res += input[i] if input[i] == input[i-offset[opt]]
   end
   res
 end
 
-def solve2(input)
-  result = 0
-  x = input.length / 2
-  len = input.length
-  input.split('').each.with_index do |el, i|
-    if el == input[(i+x)%len] then
-      result = result + el.to_i
-    end
-  end
-  result
-end
+input = prep(File.read("1.input"))
 
-# Testing and results
-input = File.read("1.input").strip
-
-puts "*** 1. Test ***"
-f = lambda {|x| solve(x)}
+puts "*** Test ***"
+puts "1:"
+f = lambda {|x| solver(x, :part1)}
 test("1122", 3, f)
 test("1111", 4, f)
 test("1234", 0, f)
 test("91212129", 9, f)
-
-puts "*** 1. Result ***"
-puts solve(input)
-
-puts
-puts "*** 2. Test ***"
-f = lambda {|x| solve2(x)}
+puts "2:"
+f = lambda {|x| solver(x, :part2)}
 test("1212", 6, f)
 test("1221", 0, f)
 test("123425", 4, f)
 test("123123", 12, f)
 test("12131415", 4, f)
 
-puts "*** 2. Result ***"
-puts solve2(input)
+puts
+puts "*** Result ***"
+puts "1: #{solver(input, :part1)}"
+puts "2: #{solver(input, :part2)}"
