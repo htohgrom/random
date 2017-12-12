a54686c4d38bab6aaba55d75871e85bd05f336d0
Krystian Wilk Pasterczyk <htohgrom+git@gmail.com>
2017-12-12 22:37:35 +0100

    aoc2017: New solutions - 11 and partial 7
    First part of day 7, but needs to be re-done to finish part 2.
    Day 11 works fine, even if ugly.

--- aoc2017/07.rb
+++ aoc2017/07.rb
@@ -1,2 +1,29 @@
 #!/usr/bin/env ruby -w
 
+input = File.read("data/07.txt").strip.split("\n")
+p input.length
+
+# Leave only processes carring other processes
+carrying = input.select {|s| s.include?("->")}
+p carrying.length
+
+procs = []
+nextprocs = []
+carrying.each do |proc|
+  dupa = proc.split("->")
+  procname = dupa[0].split[0]
+
+  procs << procname
+
+  nextprocs << dupa[1].gsub(",", "").split
+end
+
+nextprocs = nextprocs.flatten.uniq
+
+puts "procs count: #{procs.length}"
+puts "nextprocs count: #{nextprocs.length}"
+
+procs = procs.reject {|p| nextprocs.include?(p)}
+p procs
+
+# This is extremely ugly brute-force and doesn't help a bit with part 2.
--- aoc2017/11.rb
+++ aoc2017/11.rb
@@ -1,2 +1,51 @@
 #!/usr/bin/env ruby -w
+#
+# Hex bible:
+# https://www.redblobgames.com/grids/hexagons/
 
+# Using axial coordinates
+directions = {
+  "n"  => lambda {|x, y| [x,   y+1]},
+  "ne" => lambda {|x, y| [x+1, y  ]},
+  "se" => lambda {|x, y| [x+1, y-1]},
+  "s"  => lambda {|x, y| [x,   y-1]},
+  "sw" => lambda {|x, y| [x-1, y  ]},
+  "nw" => lambda {|x, y| [x-1, y+1]}
+}
+
+# Distance from [0, 0] in axial coordinates
+def dist(x, y)
+  z = -x-y
+  [x.abs, y.abs, z.abs].max
+end
+
+test1 = ["ne", "ne", "ne"]
+test2 = ["ne", "ne", "sw", "sw"]
+test3 = ["ne", "ne", "s", "s"]
+test4 = ["se", "sw", "se", "sw", "sw"]
+
+[test1, test2, test3, test4].each do |test|
+  p test
+  pos = [0, 0]
+  test.each do |step|
+    print "#{pos} : #{step} -> "
+    pos = directions[step].call(*pos)
+    puts "#{pos}"
+  end
+  p pos
+  p dist(*pos)
+  puts "*****"
+end
+
+input = File.read("data/11.txt").strip.split(",")
+pos = [0, 0]
+max = 0
+
+input.each do |step|
+  pos = directions[step].call(*pos)
+  max = [max, dist(*pos)].max
+end
+
+p pos
+p dist(*pos)
+p max
